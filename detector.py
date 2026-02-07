279e9c406ba6f13b36430887d74d0a51882c0c1a
Faheem Rahman <fxr375@student.bham.ac.uk>
2026-02-07 12:10:42 +0000

    Added detection code

--- detector.py
+++ detector.py
@@ -5,9 +5,16 @@ import time
 #Dictionary to store packet counts per IP address
 packet_counts = defaultdict(int)
 
+#For detection
+
+WINDOW = 1 # second
+THRESHOLD = 100 # packets per window
+
+window_start = time.time()
+
 #Prints information every 5 seconds
-REPEAT_INTERVAL = 5
-last_report_time = time.time()
+#REPEAT_INTERVAL = 5
+#last_report_time = time.time()
 
 
 
@@ -22,17 +29,14 @@ def packet_handler(packet):
 
 
     #prints stats every (REPEAT_INTERVAL) SECONDS
-    if current_time - last_report_time >= REPEAT_INTERVAL:
-        print("\n--- Traffic stats (last {} seconds) ---",format(REPEAT_INTERVAL))
-
+    if current_time - window_start >= WINDOW:
         for ip, count in packet_counts.items():
-            print(f"{ip}: {count} packets")
+            if count > THRESHOLD:
+                print(f"[ALERT] Possible DOS detected from {ip} ({count} packets/sec)")
 
-        print("------------------------------------")
-
-        #Clear for the next interval
+        #Reset counting 
         packet_counts.clear()
-        last_report_time = current_time
+        window_start = current_time
 
 print("Starting traffic monitoring, press CTRL + C to abort")
 sniff(prn=packet_handler, store=False)
